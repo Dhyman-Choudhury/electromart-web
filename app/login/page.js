7a8b308a0802a846ad66ddd255adad3d38e8b5bd
Dhyman Choudhury <dhyman2029@gmail.com>
2025-08-23 10:43:29 +0600

    added goolge

--- app/login/page.js
+++ app/login/page.js
@@ -8,17 +8,23 @@ export default function LoginPage() {
   const callbackUrl = searchParams.get("callbackUrl") || "/products"; // fallback
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 w-96 h-96 shadow-2xl">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Login</h1>
 
       {/* Google Login */}
+      <button 
+        onClick={() => signIn("google", { callbackUrl })}
+        className="bg-blue-500 text-white px-4 py-2 rounded mb-3"
+      >
+        Sign in with Google
+      </button>
 
       {/* Static Admin Login */}
       <button 
         onClick={() => signIn("credentials", { 
           username: "admin", password: "pass123", callbackUrl 
         })}
-        className="btn_primary px-4 py-2 rounded-lg"
+        className="bg-green-500 text-white px-4 py-2 rounded"
       >
         Sign in as Admin
       </button>
